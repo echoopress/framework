00800093dbbd93f909a1c7ccdcffbe7f31f8fe77
ZhangCheng <zhangchengx@gmail.com>
2017-01-02 18:24:38 -0500

    container singleton instance

--- system/vendor/echoopress/framework/Container.php
+++ system/vendor/echoopress/framework/Container.php
@@ -9,5 +9,15 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class Container extends ContainerBuilder
 {
+    private static $instance = NULL;
 
+    public static function getInstance()
+    {
+        if (is_null(self::$instance))
+        {
+            $c = __CLASS__;
+            self::$instance = new $c();
+        }
+        return self::$instance;
+    }
 }
