5c282dd457a343004c617f09e1078b4894b71156
ZhangCheng <zhangchengx@gmail.com>
2017-01-02 18:20:38 -0500

    Add templating for framework

---
+++ system/vendor/echoopress/framework/Templating.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Echoopress\Framework;
+
+use Symfony\Component\Templating\PhpEngine;
+use Symfony\Component\Templating\TemplateNameParser;
+use Symfony\Component\Templating\Loader\FilesystemLoader;
+
+
+class Templating extends PhpEngine
+{
+    public function __construct(TemplateNameParserInterface $parser = null, LoaderInterface $loader = null, $helpers = array())
+    {
+        if (null === $parser) {
+            $parser = new TemplateNameParser();
+        }
+        if (null === $loader) {
+            $loader = new FilesystemLoader(SYSTEM_PATH.'cache/%name%');
+        }
+        parent::__construct($parser, $loader, $helpers);
+    }
+}
