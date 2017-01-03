4af3824fd93593a65d93fe2322b46f6fa33b51df
ZhangCheng <zhangchengx@gmail.com>
2017-01-03 17:39:15 -0500

    Testing

--- public/index.php
+++ public/index.php
@@ -5,7 +5,6 @@
 define('START_TIME', microtime(true));
 define('DEBUG', true);
 define('SITE_PATH',__DIR__.'/');
-
 define('SYSTEM_PATH',__DIR__.'/../system/');
 require_once SYSTEM_PATH.'vendor/autoload.php';
 
@@ -14,14 +13,10 @@ $app = new \Echoopress\Framework\Application();
 //$app->get('/', function() {
 //    return 'home page';
 //});
-//$app->get('/test', function() {
-//    return 'test page';
-//});
-//$app->get('/test/{id}', function($id) {
-//    return 'test page id '.$id;
-//});
-$app->get('/', 'Homepage\HomeController::indexAction');
-$app->get('/welcome/{name}', 'Homepage\HomeController::welcomeAction');
-$app->get('/page', 'page');
+
+//$app->get('/', 'Homepage\HomeController::indexAction');
+//$app->get('/welcome/{name}', 'Homepage\HomeController::welcomeAction');
+$app->get('/', 'Homepage');
+//$app->get('/page', 'page');
 
 $app->run();
