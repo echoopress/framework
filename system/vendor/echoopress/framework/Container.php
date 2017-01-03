4976bc7b85f38db38d6e7adc430d3b474b0fca2f
ZhangCheng <zhangchengx@gmail.com>
2017-01-03 17:35:58 -0500

    Add helper functions for package

--- system/vendor/echoopress/framework/Container.php
+++ system/vendor/echoopress/framework/Container.php
@@ -11,13 +11,39 @@ class Container extends ContainerBuilder
 {
     private static $instance = NULL;
 
+    /**
+     * Get Container instance, singleton pattern.
+     *
+     * @return Container instance
+     */
     public static function getInstance()
     {
-        if (is_null(self::$instance))
-        {
+        if (is_null(self::$instance)) {
             $c = __CLASS__;
             self::$instance = new $c();
         }
         return self::$instance;
+    }
+
+    /**
+     * Registers a service definition.
+     *
+     * @param $package string
+     * @return \Symfony\Component\DependencyInjection\Definition A Definition instance
+     */
+    public function registerPackage($package)
+    {
+        return $this->register('package_'.$package, $package.'\Package');
+    }
+
+    /**
+     * Gets a service.
+     *
+     * @param $package string
+     * @return object The associated service
+     */
+    public function getPackage($package)
+    {
+        return $this->get('package_'.$package);
     }
 }
