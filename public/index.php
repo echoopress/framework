d869898f99edf5eeb012193996e54406b70c39ea
ZhangCheng <zhangchengx@gmail.com>
2017-01-04 22:21:45 -0500

    Clear up index.php

--- public/index.php
+++ public/index.php
@@ -10,13 +10,8 @@ require_once SYSTEM_PATH.'vendor/autoload.php';
 
 $app = new \Echoopress\Framework\Application();
 
-//$app->get('/', function() {
-//    return 'home page';
-//});
-
 //$app->get('/', 'Homepage\HomeController::indexAction');
 //$app->get('/welcome/{name}', 'Homepage\HomeController::welcomeAction');
-$app->get('/', 'Homepage');
-//$app->get('/page', 'page');
+$app->get('/', 'Demo');
 
 $app->run();
